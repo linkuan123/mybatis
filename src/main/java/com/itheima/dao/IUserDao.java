1977810e000ffb1ce8c7dbe8aff9ba306b05c789
K <45218419+linkuan123@users.noreply.github.com>
2019-10-11 23:31:40 +0800

    crud操作

--- src/main/java/com/itheima/dao/IUserDao.java
+++ src/main/java/com/itheima/dao/IUserDao.java
@@ -1,5 +1,6 @@
 package com.itheima.dao;
 
+import com.itheima.domain.QueryVo;
 import com.itheima.domain.User;
 
 import java.util.List;
@@ -17,4 +18,49 @@ public interface IUserDao {
      * @return
      */
     List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     * @return 影响数据库记录的行数
+     */
+    int updateUser(User user);
+
+    /**
+     * 根据Id删除用户
+     * @param userId
+     */
+    void deleteUser(Integer userId);
+
+    /**
+     * 根据id查询用户
+     * @param userId
+     */
+    User findById(Integer userId);
+
+    /**
+     * 通过名称模糊查询用户信息
+     * @param username
+     * @return
+     */
+    List<User> findByName(String username);
+
+    /**
+     * 聚合查询：查询总用户数
+     * @return
+     */
+    int findTotal();
+
+    /**
+     * 根据queryVo中的条件查询用户
+     * @param vo
+     * @return
+     */
+    List<User> findUserByVo(QueryVo vo);
 }
--- src/test/java/com/itheima/test/MybatisTest.java
+++ src/test/java/com/itheima/test/MybatisTest.java
@@ -1,13 +1,18 @@
 package com.itheima.test;
 
 import com.itheima.dao.IUserDao;
+import com.itheima.domain.QueryVo;
 import com.itheima.domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,27 +22,127 @@ import java.util.List;
  */
 public class MybatisTest {
 
+    private InputStream in;
+    private SqlSession sqlSession;
+    private IUserDao userDao;
+
+    @Before//用于在测试方法执行之前执行
+    public void init()throws Exception{
+        //1.读取配置文件，生成字节输入流
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.获取SqlSessionFactory
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3.获取SqlSession对象
+        sqlSession = factory.openSession();
+        //4.获取dao的代理对象
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
+
+    @After//用于在测试方法执行之后执行
+    public void destroy()throws Exception{
+        //提交事务
+        sqlSession.commit();
+        //6.释放资源
+        sqlSession.close();
+        in.close();
+    }
+
     /**
-     * 入门案例
-     * @param args
+     * 测试查询所有
      */
-    public static void main(String[] args)throws Exception {
-        //1.读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
-        //2.创建SqlSessionFactory工厂
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
-        //3.使用工厂生产SqlSession对象
-        SqlSession session = factory.openSession();
-        //4.使用SqlSession创建Dao接口的代理对象
-        IUserDao userDao = session.getMapper(IUserDao.class);
-        //5.使用代理对象执行方法
+    @Test
+    public void testFindAll(){
+        // 5、执行查询所有方法
         List<User> users = userDao.findAll();
         for(User user : users){
             System.out.println(user);
         }
-        //6.释放资源
-        session.close();
-        in.close();
+    }
+
+    /**
+     * 测试保存操作
+     */
+    @Test
+    public void testSave(){
+        User user = new User();
+        user.setUserName("modify User property");
+        user.setUserAddress("陕西西安");
+        user.setUserSex("男");
+        user.setUserBirthday(new Date());
+        System.out.println("操作保存前" + user);
+        // 5、执行保存方法
+        userDao.saveUser(user);
+
+        System.out.println("保存之后" + user);
+    }
+
+    /**
+     * 测试更新操作
+     */
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setUserId(54);
+        user.setUserName("mybastis update user");
+        user.setUserAddress("北京市顺义区");
+        user.setUserSex("女");
+        user.setUserBirthday(new Date());
+
+        userDao.updateUser(user);
+    }
+
+    /**
+     * 测试删除操作
+     */
+    @Test
+    public void testDelete(){
+        userDao.deleteUser(54);
+    }
+
+    /**
+     * 测试查找一个方法
+     */
+    @Test
+    public void testFindById(){
+        User user = userDao.findById(50);
+        System.out.println(user);
+
+    }
+
+    /**
+     * 通过名字模糊查询
+     */
+    @Test
+    public void testFindByName(){
+        List<User> users = userDao.findByName("%王%");
+        for(User user : users){
+            System.out.println(user);
+        }
+    }
+
+    /**
+     * 测试查询总记录条数
+     */
+    @Test
+    public void testFindTotal(){
+        //5.执行查询一个方法
+        int count = userDao.findTotal();
+        System.out.println(count);
+    }
+
+    /**
+     * 测试使用QueryVo作为查询条件
+     */
+    @Test
+    public void testFindByVo(){
+        QueryVo vo = new QueryVo();
+        User user = new User();
+        user.setUserName("%王%");
+        vo.setUser(user);
+        //5.执行查询一个方法
+        List<User> users = userDao.findUserByVo(vo);
+        for(User u : users){
+            System.out.println(u);
+        }
     }
 }
