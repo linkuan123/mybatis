1fb83e3d9e3485df7f4b6852c32f1b83956b6659
K <45218419+linkuan123@users.noreply.github.com>
2019-10-16 16:58:26 +0800

    一对一查询

--- src/main/java/com/itheima/domain/User.java
+++ src/main/java/com/itheima/domain/User.java
@@ -9,60 +9,60 @@ import java.util.Date;
  */
 public class User implements Serializable {
 
-    private Integer userId;
-    private String userName;
-    private String userAddress;
-    private String userSex;
-    private Date userBirthday;
+    private Integer id;
+    private String username;
+    private String address;
+    private String sex;
+    private Date birthday;
 
-    public Integer getUserId() {
-        return userId;
+    public Integer getId() {
+        return id;
     }
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getUserAddress() {
-        return userAddress;
+    public String getAddress() {
+        return address;
     }
 
-    public void setUserAddress(String userAddress) {
-        this.userAddress = userAddress;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
-    public String getUserSex() {
-        return userSex;
+    public String getSex() {
+        return sex;
     }
 
-    public void setUserSex(String userSex) {
-        this.userSex = userSex;
+    public void setSex(String sex) {
+        this.sex = sex;
     }
 
-    public Date getUserBirthday() {
-        return userBirthday;
+    public Date getBirthday() {
+        return birthday;
     }
 
-    public void setUserBirthday(Date userBirthday) {
-        this.userBirthday = userBirthday;
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "userId=" + userId +
-                ", userName='" + userName + '\'' +
-                ", userAddress='" + userAddress + '\'' +
-                ", userSex='" + userSex + '\'' +
-                ", userBirthday=" + userBirthday +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", address='" + address + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
                 '}';
     }
 }
--- src/test/java/com/itheima/test/MybatisTest.java
+++ src/test/java/com/itheima/test/MybatisTest.java
@@ -66,10 +66,10 @@ public class MybatisTest {
     @Test
     public void testSave(){
         User user = new User();
-        user.setUserName("modify User property");
-        user.setUserAddress("陕西西安");
-        user.setUserSex("男");
-        user.setUserBirthday(new Date());
+        user.setUsername("modify User property");
+        user.setAddress("陕西西安");
+        user.setSex("男");
+        user.setBirthday(new Date());
         System.out.println("操作保存前" + user);
         // 5、执行保存方法
         userDao.saveUser(user);
@@ -83,11 +83,11 @@ public class MybatisTest {
     @Test
     public void testUpdate(){
         User user = new User();
-        user.setUserId(54);
-        user.setUserName("mybastis update user");
-        user.setUserAddress("北京市顺义区");
-        user.setUserSex("女");
-        user.setUserBirthday(new Date());
+        user.setId(54);
+        user.setUsername("mybastis update user");
+        user.setAddress("北京市顺义区");
+        user.setSex("女");
+        user.setBirthday(new Date());
 
         userDao.updateUser(user);
     }
@@ -138,7 +138,7 @@ public class MybatisTest {
     public void testFindByVo(){
         QueryVo vo = new QueryVo();
         User user = new User();
-        user.setUserName("%王%");
+        user.setUsername("%王%");
         vo.setUser(user);
         //5.执行查询一个方法
         List<User> users = userDao.findUserByVo(vo);
@@ -153,7 +153,7 @@ public class MybatisTest {
     @Test
     public void testFindUserByCondition(){
         User u = new User();
-        u.setUserName("老王");
+        u.setUsername("老王");
         // u.setUserSex("男");
         List<User> users = userDao.findUserByCondition(u);
         for(User user : users){
